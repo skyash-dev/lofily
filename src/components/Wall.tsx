7ee7ea52e23c44de0e4f673c88f35902f69834be
CHAMP010 <yashpc09@gmail.com>
2024-08-09 18:03:07 +0530

    sfx

--- src/components/Wall.tsx
+++ src/components/Wall.tsx
@@ -1,42 +1,57 @@
 import { useState } from "react";
-import { getGifs, getNoise } from "../utils/constants";
-import { ChevronsLeft, ChevronsRight, ShuffleIcon } from "lucide-react";
+import { getGifs, getLinks, getNoise } from "../utils/constants";
+import {
+  ChevronsLeft,
+  ChevronsRight,
+  ShuffleIcon,
+  Play,
+  Pause,
+} from "lucide-react";
+import noise_sfx from "./../assets/noise_sfx.mp3";
 
 export default function () {
   const [imgIndex, setImgIndex] = useState<any>(0);
   const [gifs, setGifs] = useState<any>(getGifs());
+  const [linkIndex, setLinkIndex] = useState<any>(0);
+  const [links, setLinks] = useState<any>(getLinks());
   const [noise, setNoise] = useState<any>(false);
-  let randInt = Math.floor(Math.random() * (gifs.length + 1));
+  const [isPlaying, setIsPlaying] = useState(false);
+  let randImg = Math.floor(Math.random() * (gifs.length + 1));
+  let randLink = Math.floor(Math.random() * (links.length + 1));
 
   function forward() {
     setNoise(true);
     setTimeout(() => {
-      if (imgIndex < gifs.length - 1) {
+      if (imgIndex < gifs.length - 1 && linkIndex < links.length) {
+        setLinkIndex(linkIndex + 1);
         setImgIndex(imgIndex + 1);
       } else {
+        setLinkIndex(0);
         setImgIndex(0);
       }
       setNoise(false);
-    }, 150);
+    }, 300);
   }
   function backward() {
     setNoise(true);
     setTimeout(() => {
       if (imgIndex > 0) {
+        setLinkIndex(linkIndex - 1);
         setImgIndex(imgIndex - 1);
       } else {
         setImgIndex(gifs.length - 1);
       }
       setNoise(false);
-    }, 150);
+    }, 300);
   }
   function shuffle() {
     setNoise(true);
     setTimeout(() => {
-      setImgIndex(randInt);
-      randInt = Math.floor(Math.random() * (gifs.length + 1));
+      setLinkIndex(randLink);
+      setImgIndex(randImg);
+      randImg = Math.floor(Math.random() * (gifs.length + 1));
       setNoise(false);
-    }, 150);
+    }, 300);
   }
 
   return (
@@ -44,18 +59,41 @@ export default function () {
       <div className="ui">
         <div className="flex">
           <button onClick={backward}>
-            <ChevronsLeft color="white" size={32}></ChevronsLeft>
+            <ChevronsLeft color="white" size={30}></ChevronsLeft>
           </button>
+          {isPlaying ? (
+            <button onClick={() => setIsPlaying(false)}>
+              <Pause color="white" size={22}></Pause>
+            </button>
+          ) : (
+            <button onClick={() => setIsPlaying(true)}>
+              <Play color="white" size={22}></Play>
+            </button>
+          )}
           <button onClick={shuffle}>
             <ShuffleIcon color="white" size={20}></ShuffleIcon>
           </button>
           <button onClick={forward}>
-            <ChevronsRight color="white" size={32}></ChevronsRight>
+            <ChevronsRight color="white" size={30}></ChevronsRight>
           </button>
         </div>
+        <iframe
+          src={links[linkIndex]}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+        ></iframe>
       </div>
-      {noise ? <img src={getNoise()} alt="" /> : null}
-      <img src={gifs[imgIndex]} alt="" />
+      {noise ? (
+        <img src={getNoise()} alt="" />
+      ) : (
+        <img src={gifs[imgIndex]} alt="" />
+      )}
+      {noise ? (
+        <audio autoPlay>
+          <source src={noise_sfx} />
+        </audio>
+      ) : null}
       <div id="crt-lines"></div>
       <div id="darken"></div>
       <div id="vignette"></div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,6 +9,11 @@ import img8 from "./../assets/gifs/8.gif";
 import img9 from "./../assets/gifs/9.gif";
 import img10 from "./../assets/gifs/10.gif";
 import img11 from "./../assets/gifs/11.gif";
+import img12 from "./../assets/gifs/12.gif";
+import img13 from "./../assets/gifs/13.gif";
+import img14 from "./../assets/gifs/14.gif";
+import img15 from "./../assets/gifs/15.gif";
+import img16 from "./../assets/gifs/16.gif";
 
 import noise1 from "./../assets/noise1.gif";
 import noise2 from "./../assets/noise2.gif";
@@ -20,7 +25,25 @@ import noise7 from "./../assets/noise7.gif";
 import noise8 from "./../assets/noise8.gif";
 
 export function getGifs() {
-  return [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+  let gifs = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+  ];
+  return gifs;
 }
 export function getNoise() {
   let noises = [noise1, noise2, noise3, noise4, noise5, noise6, noise7, noise8];
@@ -28,4 +51,24 @@ export function getNoise() {
   let noise = noises[rand];
 
   return noise;
+}
+
+export function getLinks() {
+  let links = [
+    "https://www.youtube.com/embed/wkhLHTmS_GI?si=Ox_GmT_PIkMx9AaU?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/5yx6BWlEVcY?si=tqP2mgvp5433_GLL?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/rPjez8z61rI?si=B6DIMnksTyjIBl9r?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/rZCO-sAtP0I?si=HimrfgJnWmcVBXbt?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/TfmECBzmOn4?si=m_IFmQMahf98O9QA?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/A_hmrykwR7g?si=8gT54-Zokvj5pw0k?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/8Yqx3we3WJ4?si=doom7xT9AABLwPq_?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/7NOSDKb0HlU?si=7L8_Pk2bNx3hWccs?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/C_9FdFnE5hY?si=s-7O_t0kgCMmwrd9?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/wJSg1H8wOUg?si=W_WNkYta99X2yxBP?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/qH3fETPsqXU?si=QomLWangWk1wpEUi?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/tNkZsRW7h2c?si=cbWNjmvaDPuzz-11?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+    "https://www.youtube.com/embed/lP26UCnoH9s?si=9lxEp2NukIl3tgzI?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fwww.lofi.cafe&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=true&color=black&enablejsapi=1&widgetid=1",
+  ];
+
+  return links;
 }
